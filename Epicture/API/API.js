548d18f5c00974bb49f2cd70fcd59354842e6ff6
yadaro <yadaro.yun@epitech.eu>
2018-11-18 15:01:38 +0100

    push searchImg function

--- Epicture/API/API.js
+++ Epicture/API/API.js
@@ -12,6 +12,20 @@ export function getContent(page) {
 	.catch((err) => console.error(err))
 }
 
+export function searchImg(query, sort, window) {
+
+	// var url = "https://api.imgur.com/3/gallery/search/time/1?q=" + query
+	var url = "https://api.imgur.com/3/gallery/search/" + sort + "/" + window + "/1?q=" + query
+	console.log("Search")
+	return fetch(url, {
+		headers: {
+			'Authorization': "Client-ID " + API_TOKEN
+		}
+	})
+	.then((response) => response.json())
+	.catch((err) => console.error(err))
+}
+
 export function getConnectionURL() {
 	const url = "https://api.imgur.com/oauth2/authorize?client_id=" + API_TOKEN + "&response_type=token"
 	return (url)
